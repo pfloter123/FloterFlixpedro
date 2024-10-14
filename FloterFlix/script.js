52007f122aebd116d07756b0891128cba325e952
pfloter123 <pedrooliveirafloter0@gmail.com>
2024-10-14 19:18:01 -0300

    subindo arquivos

---
+++ FloterFlix/script.js
@@ -0,0 +1,5 @@
+// Espera 3 segundos antes de redirecionar
+setTimeout(function() {
+    // Redireciona para a página desejada
+    window.location.href = 'tela_inicial.html';
+}, 3000); // 3000 milissegundos = 3 segundos
